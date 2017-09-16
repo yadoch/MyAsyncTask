f85f8e9fcb15b6bcdfacca16f115699a09ee1fa5
yadoch <yadoch@gmail.com>
2017-09-16 13:04:33 +0800

    傳入字串範例和onProgressUpdate 傳值數值範例.

--- app/src/main/java/tw/com/abc/myasynctask/MainActivity.java
+++ app/src/main/java/tw/com/abc/myasynctask/MainActivity.java
@@ -8,15 +8,16 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    //private TextView tv;
+    private TextView tv;
     private MyAsyncTask myAsyncTask;
+    private int i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //  tv=(TextView) findViewById(R.id.tv);
+        tv=(TextView) findViewById(R.id.tv);
 
     }
 
@@ -25,9 +26,16 @@ public class MainActivity extends AppCompatActivity {
         myAsyncTask.execute("brad","TCCA","OK");
         //
     }
-    private class MyAsyncTask extends AsyncTask<String, Void, Void> {
+    private class MyAsyncTask extends AsyncTask<String,Integer, Void> {
     //private class MyAsyncTask extends AsyncTask<Void, Void, Void> {
+/*
+        @Override
+        protected void onPostExecute(String result) {
 
+            super.onPostExecute(result);
+            Log.i("geoff", "onPostExecute()!! ");
+        }
+*/
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
@@ -35,11 +43,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onProgressUpdate(Void... values) {
+        protected void onProgressUpdate(Integer... values) {
             super.onProgressUpdate(values);
-            Log.i("geoff", "onProgressUpdate()!! ");
+            StringBuffer sb = new StringBuffer();
+            for (Integer i : values) {
+                sb.append(i+" \n");
+        //        Log.i("geoff", "onProgressUpdate()!!: ");
+            }
+            tv.setText(sb);
         }
-
         @Override
         protected void onCancelled(Void aVoid) {
             super.onCancelled(aVoid);
@@ -56,8 +68,10 @@ public class MainActivity extends AppCompatActivity {
         //protected Void doInBackground(Void... params) {
         protected Void doInBackground(String... params) {
             for(String name : params) {
-
+                i++;
                 Log.i("geoff", "doInBackground()!!"+name);
+                //會觸發 onProgressUpdate() 的生命週期
+                publishProgress(i,i+10,i+100);
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
